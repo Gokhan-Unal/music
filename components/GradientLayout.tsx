223339a5a58e0f921814775c2f8d3c5920ab18b3
Gökhan Ünal <g.unal@emakina.com.tr>
2022-07-03 14:24:30 +0300

    Image and Text

--- components/GradientLayout.tsx
+++ components/GradientLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image } from '@chakra-ui/react';
-import { Box, Flex } from '@chakra-ui/layout';
+import { Box, Flex, Text } from '@chakra-ui/layout';
 
 export default function GradientLayout({ color, children, image, subtitle, title, description, roundImage }) {
   return (
@@ -11,7 +11,22 @@ export default function GradientLayout({ color, children, image, subtitle, title
     >
       <Flex bg={`${color}.600`} padding='40px' align='end'>
         <Box padding='20px'>
-          <Image boxSize='160px' boxShadow='2xl' src={image} borderRadius={roundImage ? '100%' : '3px'} />
+          <Image
+            boxSize='160px'
+            boxShadow='2xl'
+            src={image}
+            borderRadius={roundImage ? '100%' : '3px'}
+            objectFit='cover'
+          />
+        </Box>
+        <Box padding='20px' lineHeight='40px' color='white'>
+          <Text fontSize='sm' fontWeight='bold' casing='uppercase'>
+            {subtitle}
+          </Text>
+          <Text fontSize='6xl'>{title}</Text>
+          <Text fontSize='sm' fontWeight='100'>
+            {description}
+          </Text>
         </Box>
       </Flex>
     </Box>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,14 @@ import GradientLayout from '../components/GradientLayout';
 
 export default function Home() {
   return (
-    <GradientLayout color='red' roundImage>
+    <GradientLayout
+      color='green'
+      title='Gökhan Ünal'
+      subtitle='Profile'
+      description='15 Playlists'
+      image='https://i.pinimg.com/originals/db/b0/f0/dbb0f0ede2520c4320bd909b360977a0.png'
+      roundImage
+    >
       <div>Home Page</div>
     </GradientLayout>
   );
